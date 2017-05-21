333ce13d64eac065f82113547ab7b1d9dd4c83c7
barrygahan <barrygahan@barrys-imac.lan>
2017-05-21 20:59:07 +0100

    Adding a 3rd line

--- TestProject/src/com/roamingrascals/HelloWorld.java
+++ TestProject/src/com/roamingrascals/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main (String args[]) {
 		System.out.println("Hello world from Roaming Rascals 24");
 		System.out.println("Sta	rting of development");
+		System.out.println("End of dev");
 	}
 }
